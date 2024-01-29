32a35077b591aebed7126df55b7ab94007dbc7c6
vinz <vincent.gardebien@epitech.eu>
2024-01-29 11:19:12 +0400

    step 7

--- Parse.hs
+++ Parse.hs
@@ -1,4 +1,11 @@
+import Data.Char (isDigit)
+
 filterArr :: (a -> Bool) -> [a] -> [a]
 filterArr _ [] = []
 filterArr f (x:xs) | f x = x : filterArr f xs
                    | otherwise = filterArr f xs
+
+parseLine :: [String] -> [String]
+parseLine xs = filter (not . isNum) xs ++ filter isNum xs
+  where
+    isNum = all isDigit
