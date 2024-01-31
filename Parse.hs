ac83742556a966480921e2edb2df3c7c46dcfb28
vinz <vincent.gardebien@epitech.eu>
2024-01-31 07:58:42 +0400

    correct Parse + raedme

--- Parse.hs
+++ Parse.hs
@@ -1,11 +1,19 @@
 import Data.Char (isDigit)
 
+isNum :: String -> Bool
+isNum [] = False
+isNum = all isDigit
+
+isOperator :: String -> Bool
+isOperator [] = False
+isOperator = (`elem` ["+", "-", "*", "/"])
+
+
 filterArr :: (a -> Bool) -> [a] -> [a]
 filterArr _ [] = []
 filterArr f (x:xs) | f x = x : filterArr f xs
                    | otherwise = filterArr f xs
 
 parseLine :: [String] -> [String]
+parseLine [] = []
 parseLine xs = filter (not . isNum) xs ++ filter isNum xs
-  where
-    isNum = all isDigit
